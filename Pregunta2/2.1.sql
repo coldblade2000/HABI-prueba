97653d6b898602f808225a9153349dd864d1c9ce
coldblade2000 <diegogranada7@gmail.com>
2022-09-21 12:28:26 -0500

    Acabe la 2.1

--- Pregunta2/2.1.sql
+++ Pregunta2/2.1.sql
@@ -1,3 +1,7 @@
-SELECT *
-FROM "table-BEBEDOR"
-JOIN
+SELECT beb.cedula, beb.nombre
+FROM "table-BEBEDOR" as "beb"
+LEFT OUTER JOIN "table-GUSTA" AS "gusta"
+    ON beb.cedula = "gusta".cedula
+LEFT OUTER JOIN "table-BEBIDA" bebida
+    ON gusta."codigo-bebida" = bebida."codigo-bebida"
+WHERE bebida."nombre-bebida" IS NULL OR LOWER(bebida."nombre-bebida") NOT LIKE '%colombiana%'

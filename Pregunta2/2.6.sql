192b00b9d9cc4ae1ff9382c6f9614000daa62ece
coldblade2000 <diegogranada7@gmail.com>
2022-09-21 23:15:38 -0500

    Acabe 2.6

--- Pregunta2/2.6.sql
+++ Pregunta2/2.6.sql
@@ -1,14 +1,17 @@
+-- Los bebedores que unicamente frecuentan las
+-- tiendas que unicamente sirven alguna bebida que le gusta
+
 SELECT *
 FROM "table-BEBEDOR" bebedor
 JOIN "table-FRECUENTA" frec1 on bebedor.cedula = frec1.cedula
 JOIN "table-GUSTA" gusta1 on bebedor.cedula = gusta1.cedula
 JOIN "table-VENDE" vende1 on frec1."codigo-tienda" = vende1."codigo-tienda"
-     AND vende1."codigo-bebida" = gusta1."codigo-bebida"
+    AND vende1."codigo-bebida" = gusta1."codigo-bebida"
 WHERE 1 = (
     SELECT COUNT("codigo-bebida")
     FROM "table-VENDE" vende2
     WHERE vende2."codigo-tienda" = vende1."codigo-tienda"
-    )
+)
 AND 1 = (
     SELECT COUNT(frec2."codigo-tienda")
     FROM "table-FRECUENTA" frec2
